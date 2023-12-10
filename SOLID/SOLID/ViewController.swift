1381c3b28a7e01c0f84650f86f1a884ab9d9d1d3
hieubui1006 <hieubui.hbv1006@gmail.com>
2023-12-10 13:18:17 +0700

    create project

---
+++ SOLID/SOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID
+//
+//  Created by mrhb on 10/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
